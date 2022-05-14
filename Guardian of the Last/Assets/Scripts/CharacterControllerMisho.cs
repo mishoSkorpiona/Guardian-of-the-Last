d406d41fb9ac456c655f1a795568ca6ef4f90206
Mihail <misho258456@gmail.com>
2022-05-14 17:23:12 +0200

    Unity Project Created

---
+++ Guardian of the Last/Assets/Scripts/CharacterControllerMisho.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterControllerMisho : MonoBehaviour
+{
+    [SerializeField] private Vector3 moveDirection;
+
+    private Rigidbody _rb;
+
+    [SerializeField] private float _speed = 2;
+
+    [SerializeField] private GameObject _baseBone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _rb.AddForce(moveDirection * _speed * Time.deltaTime*500);
+        dragBottom();
+    }
+
+    void OnMove (InputValue input)
+    {
+        moveDirection.x = input.Get<Vector2>().x;
+        moveDirection.z = input.Get<Vector2>().y;
+    }
+
+    void dragBottom()
+    {
+        
+        _baseBone.transform.localPosition = -_rb.velocity.normalized * 0.01f;
+    }
+
+}
